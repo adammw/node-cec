4c5054c9b2294b2ad90e066eaa7ee17f12beac65
Adam Malcontenti-Wilson <adman.com@gmail.com>
2015-01-14 01:38:59 +1100

    Add example script

---
+++ example/cec.js
@@ -0,0 +1,29 @@
+// This example connects to the first available CEC adapter,
+// sends a ping message, powers on the devices and then powers them off
+var CEC = require('../');
+var debug = require('debug')('example');
+var cec = new CEC();
+
+var adapters = cec.detectAdapters();
+debug('adapters: %j', adapters);
+
+console.log('connecting to adapter...');
+var ret = cec.open(adapters[0].portName, function(err, adapter) {
+  if (err) throw err;
+  adapter.transmit({
+    initiator: CEC.LogicalAddress.TUNER1,
+    destination: CEC.LogicalAddress.TV
+  });
+  adapter.on('logmessage', function(data) {
+    debug(data.message);
+  });
+  setTimeout(function() {
+    console.log('powering on...');
+    adapter.powerOn();
+  }, 1000);
+  setTimeout(function() {
+    console.log('powering off...');
+    adapter.standby();
+    process.exit(0);
+  }, 5000);
+});
